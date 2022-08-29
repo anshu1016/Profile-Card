a95d63b04dac21ef71bdf8168982f09cd123ecbc
Arun Shukla <arunshukla98710@gmail.com>
2022-08-29 08:32:49 +0000

    updated bio and  some text

---
+++ src/bio.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Bio() {
+  return (
+    <div className="bio">
+      <h2>Arun Shukla</h2>
+      <h4> Front End Developer - Beginner</h4>
+      <h5>
+        <a href="https://arunshukla.netlify.app/" className="bio-link">
+          arunshukla.portfolio &nbsp;
+        
+        <i className="fas fa-external-link-alt"></i>
+      </a></h5>
+    </div>
+  );
+}
