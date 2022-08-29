6c2605aa90fb97b9179230c5587e02bc3765928b
Arun Shukla <69161719+anshu1016@users.noreply.github.com>
2022-08-29 14:16:36 +0530

    Create social.js

---
+++ src/social.js
@@ -0,0 +1,21 @@
+//import { func } from "prop-types"
+import React from "react";
+export default function Social() {
+  return (
+    <div className="Social-btn">
+      <button className="btn1">
+        <a href="mailto:arunshukla98710@gmail.com" target="_blank">
+          <i className="fas fa-envelope">&nbsp;&nbsp;Email</i>
+        </a>
+      </button>
+      <button className="btn2">
+        <a
+          href="https://www.linkedin.com/in/arun-shukla-1399a9196/"
+          target="_blank"
+        >
+          <i className="fab fa-linkedin-in">&nbsp;&nbsp;LinkedIn</i>
+        </a>
+      </button>
+    </div>
+  );
+}
