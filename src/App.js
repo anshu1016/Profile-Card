37254eac65b6bc48ae73b65b87fc387476b3c856
Arun Shukla <69161719+anshu1016@users.noreply.github.com>
2022-08-29 14:10:55 +0530

    updated app file with props

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+import React from "react"
 import "./styles.css";
-
+import Profile from "./profile";
+import Bio from "./bio";
+import Social from "./social";
+import Main from "./main";
+import Footer from "./footer";
+//import Profile from "./img";
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+    <div className="container">
+      <Profile/>
+      <Bio />
+      <Social />
+      <Main />
+      <Footer />
     </div>
   );
 }
